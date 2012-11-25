6f66d2aa3f9c9f5b078b6c97f5af9475372ee65f
Kevin Ormbrek <kormbrek@gmail.com>
2012-11-25 12:19:16 -0600

    missing import

--- src/SudsLibrary/plugins.py
+++ src/SudsLibrary/plugins.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import xml.dom.minidom
 from suds.plugin import MessagePlugin
 from robot.api import logger
 
