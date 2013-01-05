814eb995081b62cff06d4dc7b97a855a8b5e1bbf
Kevin Ormbrek <kormbrek@gmail.com>
2013-01-05 12:40:20 -0600

    fix issues with get/set wsdl object

--- src/SudsLibrary/factory.py
+++ src/SudsLibrary/factory.py
@@ -18,41 +18,44 @@ from suds.sudsobject import Object as SudsObject
 class _FactoryKeywords(object):
 
     def set_wsdl_object_attribute(self, object, name, value):
-        """Sets the attribute of a WSDL object ."""
+        """Sets the attribute of a WSDL object."""
         self._assert_is_suds_object(object)
-        assert hasattr(object, name), "object has no attribute '%s'" % name
+        getattr(object, name)
         setattr(object, name, value)
 
     def get_wsdl_object_attribute(self, object, name):
         """Gets the attribute of a WSDL object.
 
-        Extendend variable syntax may be used to access attributes; however, 
-        some WSDL objects may have attribute names that are illegal in Python, 
+        Extendend variable syntax may be used to access attributes; however,
+        some WSDL objects may have attribute names that are illegal in Python,
         necessitating this keyword.
         """
         self._assert_is_suds_object(object)
         return getattr(object, name)
 
-    def create_wsdl_object(self, type, *key_value_pairs):
+    def create_wsdl_object(self, type, *name_value_pairs):
         """Creates a WSDL object of the specified `type`.
 
-        Requested `type` must be defined in the WSDL or in an import specified 
-        by the WSDL or with `Add Doctor Import`.
+        Requested `type` must be defined in the WSDL, in an import specified
+        by the WSDL, or with `Add Doctor Import`.
 
         Example:
-        | ${contact}=               | Create Wsdl Object | Contact |              |              |
-        | Set Wsdl Object Attribute | ${contact}         | Name    | Kelly Newman |              |
+        | ${contact}=               | Create Wsdl Object | Contact |              |
+        | Set Wsdl Object Attribute | ${contact}         | Name    | Kelly Newman |
+        Attribute values can be set by passing the attribute name and value in
+        pairs. This is equivalent to the two lines above:
         | ${contact}=               | Create Wsdl Object | Contact | Name         | Kelly Newman |
         """
-        if len(key_value_pairs) % 2 != 0:
+        if len(name_value_pairs) % 2 != 0:
             raise ValueError("Creating a WSDL object failed. There should be "
                              "an even number of name-value pairs.")
         obj = self._client().factory.create(type)
-        for i in range(0, len(key_value_pairs), 2):
-            self.set_attribute(obj, key_value_pairs[i], key_value_pairs[i+1])
+        for i in range(0, len(name_value_pairs), 2):
+            self.set_wsdl_object_attribute(obj, name_value_pairs[i], name_value_pairs[i + 1])
         return obj
 
     # private
 
     def _assert_is_suds_object(self, object):
-        assert isinstance(object, SudsObject), "object must be a WSDL object(suds.sudsobject.Object)"
+        if not isinstance(object, SudsObject):
+            raise ValueError("Object must be a WSDL object (suds.sudsobject.Object).")
