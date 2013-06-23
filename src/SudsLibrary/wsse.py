54dfcd79e862fb1105a8d2fc88bddc82bfbc0196
Kevin Ormbrek <kormbrek@gmail.com>
2013-06-23 18:31:38 -0500

    support PasswordDigest type passwords

--- src/SudsLibrary/wsse.py
+++ src/SudsLibrary/wsse.py
@@ -21,10 +21,12 @@ from suds.sax.element import Element
 from suds.sax.date import UTC
 from random import random
 from hashlib import sha1
+import base64
 import robot
 
 
-PASSWORDTEXT_TYPE = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText'
+TEXT_TYPE = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText'
+DIGEST_TYPE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest"
 BASE64_ENC_TYPE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary"
 WSSENS = \
     ('wsse',
@@ -48,11 +50,10 @@ class AutoUsernameToken(UsernameToken):
 
     def __init__(self, username=None, password=None, setcreated=False,
                  setnonce=False, digest=False):
-        if digest:
-            raise NotImplementedError()
         UsernameToken.__init__(self, username, password)
         self.autosetcreated = setcreated
         self.autosetnonce = setnonce
+        self.digest = digest
 
     def setnonce(self, text=None):
         if text is None:
@@ -64,29 +65,41 @@ class AutoUsernameToken(UsernameToken):
             self.nonce = text
 
     def xml(self):
+        if self.digest and self.password is None:
+            raise RuntimeError("Cannot generate password digest without the password.")
+        if self.autosetnonce:
+            self.setnonce()
+        if self.autosetcreated:
+            self.setcreated()
         root = Element('UsernameToken', ns=WSSENS)
         u = Element('Username', ns=WSSENS)
         u.setText(self.username)
         root.append(u)
         if self.password is not None:
+            password = self.password
+            if self.digest:
+                password = self.get_digest()
             p = Element('Password', ns=WSSENS)
-            p.setText(self.password)
-            p.set('Type', PASSWORDTEXT_TYPE)
+            p.setText(password)
+            p.set('Type', DIGEST_TYPE if self.digest else TEXT_TYPE)
             root.append(p)
-        if self.autosetnonce:
-            self.setnonce()
         if self.nonce is not None:
             n = Element('Nonce', ns=WSSENS)
-            n.setText(self.nonce)
+            n.setText(base64.encodestring(self.nonce)[:-1])
             n.set('EncodingType', BASE64_ENC_TYPE)
             root.append(n)
-        if self.autosetcreated:
-            self.setcreated()
         if self.created:
-            n = Element('Created', ns=WSUNS)
-            n.setText(str(UTC(self.created)))
-            root.append(n)
+            c = Element('Created', ns=WSUNS)
+            c.setText(str(UTC(self.created)))
+            root.append(c)
         return root
+
+    def get_digest(self):
+        nonce = str(self.nonce) if self.nonce else ""
+        created = str(UTC(self.created)) if self.created else ""
+        password = str(self.password)
+        message = nonce + created + password
+        return base64.encodestring(sha1(message).digest())[:-1]
 
 
 class _WsseKeywords(object):
@@ -110,20 +123,26 @@ class _WsseKeywords(object):
         wsse.tokens.insert(0, AutoTimestamp(validity))
         self._client().set_options(wsse=wsse)
 
-    def apply_username_token(self, username, password=None, setcreated=False, setnonce=False):
+    def apply_username_token(self, username, password=None, setcreated=False,
+                             setnonce=False, digest=False):
         """Applies a UsernameToken element to future requests.
 
         The SOAP header will contain a UsernameToken element as specified in
-        the WS-Security extension. The password type is PasswordText (plain
-        text). The Created and Nonce values, if enabled, are generated
-        automatically and updated every time a request is made.
+        Username Token Profile 1.1 that complies with Basic Security Profile
+        1.1. The Created and Nonce values, if enabled, are generated
+        automatically and updated every time a request is made. If `digest` is
+        True, a digest derived from the password is sent.
 
         Example:
         | Apply Username Token | ying | myPa$$word |
         """
         setcreated = to_bool(setcreated)
         setnonce = to_bool(setnonce)
-        token = AutoUsernameToken(username, password, setcreated, setnonce)
+        digest = to_bool(digest)
+        if digest and password is None:
+            raise RuntimeError("Password is required when digest is True.")
+        token = AutoUsernameToken(username, password, setcreated, setnonce,
+                                  digest)
         wsse = self._get_wsse()
         wsse.tokens = [x for x in wsse.tokens if not isinstance(x, UsernameToken)]
         wsse.tokens.append(token)
--- test/resources/Util.py
+++ test/resources/Util.py
@@ -1,5 +1,7 @@
-from suds.sax.date import UTC
-from datetime import datetime
+from suds.sax.date import UTC, DateTime, Timezone
+import base64
+from robot.libraries.BuiltIn import BuiltIn
+from SudsLibrary.wsse import AutoUsernameToken
 
 
 class Util(object):
@@ -12,3 +14,25 @@ class Util(object):
         else:
             end_dt = UTC(end).datetime
         return (end_dt - start_dt).total_seconds()
+
+    def set_fixed_nonce(self, nonce):
+        """`nonce` should be Base64 encoded."""
+        token = self._get_autousernametoken()
+        token.autosetnonce = False
+        token.setnonce(base64.decodestring(nonce))
+
+    def set_fixed_created(self, created):
+        if not isinstance(created, basestring) or created[-1].upper() != 'Z':
+            raise ValueError("only UTC xsd:datetime as string supported")
+        token = self._get_autousernametoken()
+        token.autosetcreated = False
+        # get the local adjustment
+        tz_parts = Timezone.split(str(DateTime(str(UTC()))))
+        if len(tz_parts) == 2:
+            created = created[:-1] + tz_parts[1]
+        token.setcreated(created)
+
+    def _get_autousernametoken(self):
+        sl = BuiltIn().get_library_instance('SudsLibrary')
+        wsse = sl._get_wsse(False)
+        return [token for token in wsse.tokens if isinstance(token, AutoUsernameToken)][0]
